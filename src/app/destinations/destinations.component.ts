93a98352381e1af4bb642331abb033e368a24bb8
Agnieszka Zagorna <a.h.zagorna@gmail.com>
2023-08-10 17:46:13 +0200

    added filtering by terrain

--- src/app/destinations/destinations.component.ts
+++ src/app/destinations/destinations.component.ts
@@ -12,6 +12,7 @@ export class DestinationsComponent implements OnInit {
   planets: Planet[] | null = null
   copyOfPlanets: Planet[] | null = null
   selectedClimate = '';
+  selectedTerrain = '';
   sortedAlphabetically = false;
 
   constructor(private readonly travelService: TravelService, private router : Router) {}
@@ -31,7 +32,7 @@ export class DestinationsComponent implements OnInit {
     }
   }
 
-  filterByClimate(selectedClimate: string) {
+  filter(selectedClimate: string, selectedTerrain: string) {
     this.planets = this.copyOfPlanets
     this.sortedAlphabetically = false
 
@@ -39,6 +40,13 @@ export class DestinationsComponent implements OnInit {
       this.planets = this.planets.filter((planet) => {
         const planetClimates = planet.climate.split(',').map(climate => climate.trim())
         return planetClimates.includes(selectedClimate)
+    })
+    }
+
+    if(this.planets !== null && selectedTerrain !== undefined && selectedTerrain !== '') {
+      this.planets = this.planets.filter((planet) => {
+        const planetTerrains = planet.terrain.split(',').map(terrain => terrain.trim())
+        return planetTerrains.includes(selectedTerrain)
     })
     }
   }
